5be383c9b96cd163d25a2c3b98ac2336bbc7f95c
Aravind-Magickwoods <aravindambigapathy@gmail.com>
2026-04-28 15:24:36 +0530

    fixxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-
+'use client'
 import BlogListingPage from "@/components/blogs";
 import Portfolio from "@/components/mypage";
-import { motion } from "framer-motion";
 import { useState } from "react";
 
 export default function Home() {
@@ -21,27 +20,24 @@ export default function Home() {
   ]);
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4 bg-white text-blue-900">
       <main className="w-full flex flex-col gap-8">
         <BlogListingPage />
         <section className="flex flex-col gap-8">
           <h2 className="text-3xl font-bold text-primary">Testimonials</h2>
           <div className="flex flex-col gap-4 md:flex-row md:gap-8">
             {testimonials.map((testimonial, index) => (
-              <motion.div
+              <div
                 key={index}
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
                 className="bg-secondary p-8 rounded-lg shadow-md w-full md:w-1/3"
               >
-                <p className="text-lg font-bold text-white">{testimonial.name}</p>
-                <p className="text-gray-200">{testimonial.text}</p>
-              </motion.div>
+                <p className="text-lg font-bold text-black">{testimonial.name}</p>
+                <p className="">{testimonial.text}</p>
+              </div>
             ))}
           </div>
           <div className="flex justify-center mt-8">
-            <button className="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded">
+            <button className="bg-primary hover:bg-primary-dark text-black font-bold py-2 px-4 rounded">
               Read More
             </button>
           </div>
