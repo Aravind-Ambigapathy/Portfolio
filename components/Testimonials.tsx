49fb5b46ad151adb417df1489dde0d22d659ccb6
Aravind-Magickwoods <aravindambigapathy@gmail.com>
2026-04-29 11:16:58 +0530

    feat: only change the title from projects to testimonial

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,7 +3,7 @@ import Testimonial from './Testimonial';
 const Testimonials = () => {
   return (
     <div>
-      <h1>Testimonials</h1>
+      <h1>Testimonial</h1>
       <Testimonial />
       {/* rest of the code remains the same */}
     </div>
